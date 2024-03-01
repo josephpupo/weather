ae3de093e72627a8ad4a6374860babeaaf6bb22d
josephpupo <josephedwardpupo@gmail.com>
2024-03-01 16:07:42 -0500

    Homepage UI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,10 +15,11 @@
 <body>
   <div id='main-weather'>
     <h2>Weather App!</h2>
+    <img src = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/25d45014-8cc3-4c98-b02c-5a0cf3a55ddd/dbwrfyf-3ad67c12-bf02-4231-bed9-fbb6f2b00ba5.png/v1/fill/w_900,h_900/sun_on_a_transparent_background__by_prussiaart_dbwrfyf-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9OTAwIiwicGF0aCI6IlwvZlwvMjVkNDUwMTQtOGNjMy00Yzk4LWIwMmMtNWEwY2YzYTU1ZGRkXC9kYndyZnlmLTNhZDY3YzEyLWJmMDItNDIzMS1iZWQ5LWZiYjZmMmIwMGJhNS5wbmciLCJ3aWR0aCI6Ijw9OTAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.iosIdwUZ5hFlqY7tTU35rkr2gKrVC5QbvtYgvjySbog" id="weather-icon" alt="Weather Icon">
+    <img id = "display-weather" alt = "display weather">
     <input type="text" id="city" placeholder="Enter City">
     <button on:click={getWeather}>Search</button>
 
-    <img id="weather-icon" alt="Weather Icon">
     <div id="temp-div"></div>
     <div id="weather-info"></div>
     <div id="hourly-forecast"></div>
@@ -33,13 +34,13 @@
     background-position: center;
     background-attachment: fixed;
     background-size: cover;
-
-    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
     display: flex;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
     align-items: center;
     justify-content: center;
     height: 100vh;
-    margin: 0;
+    margin: 0 auto;
+    
   }
   #main-weather{
     max-width: 400px;
@@ -49,5 +50,63 @@
     backdrop-filter: blur;
     border: 1px;
     text-align: center;
+    position: relative;
+    top: 30%;
+    margin: -10px auto;
   }
+  h2, label, p{
+    display:block;
+    color: #fff;
+    margin: 8px 0;
+  }
+
+  input {
+    width: calc(100%-16px);
+    padding: 8px;
+    box-sizing: border-box;
+    border-radius: 10px;
+    border: 1px  black;
+    margin-top: 20px;
+  }
+
+  button{
+    background: white;
+    color: black;
+    padding: 11px;
+    border: none;
+    border-radius: 10px;
+    cursor: pointer;
+    width: 100px;
+    font-size: 15px;
+    display: block;
+    margin: 20px auto;
+  }
+
+  button:hover{
+    background: gray;
+  }
+
+  #weather-icon{
+    display:block;
+    max-height: 50%;
+    max-width: 100%;
+    align-items: center;
+  }
+
+  #temp-div p{
+    font-size: 60px;
+    margin-top: -30px;
+  }
+
+  #weather-info {
+    font-size: 20px;
+  }
+
+  #display-weather {
+    width: 200px;
+    height: 200px;
+    margin: 0 auto 10px;
+    display: none;
+  }
+    
 </style>
