9476e582ca9e45357083f2d0a5540bd201485f9d
josephpupo <josephedwardpupo@gmail.com>
2024-04-02 17:35:55 -0400

    added geocoding and weather/temperature framework in page.js

--- src/routes/displayWeather/+page.js
+++ src/routes/displayWeather/+page.js
@@ -1,11 +1,23 @@
 import { PUBLIC_API_KEY } from '$env/static/public';
-import OpenWeatherMap from 'openweathermap-ts';
+// import OpenWeatherMap from 'openweathermap-ts';
+export const ssr = false;
 
 export const load = async ({ fetch }) => {
+    const limit = 5;
+    // Geocoding
+    const geoRes = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=East+Meadow&limit=${5}&appid=${PUBLIC_API_KEY}`);
+    const geoData = await geoRes.json();
+    const geo = geoData;
+    console.log(geo);
+    
+    // Weather and Temperature Data
     const weatherRes = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=40.714161&lon=-73.559502&appid=${PUBLIC_API_KEY}`)
     const weatherData = await weatherRes.json();
     const weather = weatherData.weather;
+    const temp = weatherData.main.temp;
     return {
-        weather: weather
+        weather: weather,
+        temp: temp,
+        geo: geo
     }
 }
--- src/routes/displayWeather/+page.svelte
+++ src/routes/displayWeather/+page.svelte
@@ -1,12 +1,11 @@
 <script>
+  // export const ssr = false;
   export let data;
+  console.log(data);
   const weather = data.weather;
+  const temp = data.temp;
   let entries = Object.entries(weather[0])
   console.log(weather[0]);
+  console.log(temp);
 </script>
 
-{#each entries as [key, value]}
-  <div>
-    {key} - {value}
-  </div>
-{/each}
